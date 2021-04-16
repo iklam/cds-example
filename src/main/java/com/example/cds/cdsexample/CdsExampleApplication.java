97c709d27f190ffaf6e66de75e85715f1ad137ad
William Korando <wkorando@gmail.com>
2021-04-16 15:04:15 -0500

    Initial import and adding spring boot project

---
+++ src/main/java/com/example/cds/cdsexample/CdsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cds.cdsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cds/cdsexample/CdsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cds.cdsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
